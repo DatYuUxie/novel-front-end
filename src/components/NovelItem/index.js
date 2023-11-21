5b79ff9087f2a2e5ca41a1915f2426b77d71f69a
DatYuUxie <118263835+DatYuUxie@users.noreply.github.com>
2023-11-21 23:06:58 +0700

    novel item component

--- src/components/NovelItem/index.js
+++ src/components/NovelItem/index.js
@@ -1,9 +1,34 @@
+import classNames from 'classnames/bind';
+import styles from './NovelItem.module.scss';
+import Button from '../Button';
+
+const cx = classNames.bind(styles);
+
 function NovelItem() {
     return (
-        <div>
-            
+        <div className={cx('container')}>
+            <div className={cx('novel-item-container')}>
+                <div className={cx('cover-novel-item')}>
+                    <div className={cx('cover-novel-img')}>
+                        <img
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhegpPNPQyfEAJtmb5QFa3gjZZjHjVRpOCWA&usqp=CAU"
+                            alt="cover-img"
+                            className={cx('cover-img')}
+                        />
+                    </div>
+                    <div className={cx('novel-info')}>
+                        <h3 className={cx('name')}>Shadow Slave</h3>
+                        <p className={cx('novel-des')}>
+                            Growing up in poverty, Sunny never expected anything good from life. However, even he did
+                            not anticipate being chosen by the Nightmare Spell and becoming However, even he did not
+                            anticipate being chosen by the Nightmare Spell and becoming
+                        </p>
+                        <h5 className={cx('type')}>fantasty</h5>
+                    </div>
+                </div>
+            </div>
         </div>
-      );
+    );
 }
 
 export default NovelItem;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,24 @@
 import Banner from "../../components/Banner";
+import NovelItem from "../../components/NovelItem";
+import classNames from 'classnames/bind';
+import styles from './Home.module.scss';
 
+const cx = classNames.bind(styles);
 
 function Home() {
     return (
         <>
         <Banner/>
+        <div className={cx('container')}>
+        <NovelItem/>
+        <NovelItem/>
+        <NovelItem/>
+        <NovelItem/>
+        </div>
+        
+        
+        
+
         </>
     );
 }
