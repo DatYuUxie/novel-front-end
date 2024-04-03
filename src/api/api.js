38bbc087f789f37594f2ed674debd7d4ed02fd4f
quangkhai482002 <118819349+quangkhai482002@users.noreply.github.com>
2024-04-03 11:28:39 +0700

    trycatch_api

--- src/api/api.js
+++ src/api/api.js
@@ -2,16 +2,42 @@ import axios from 'axios';
 
 // ===========   user api   ===========
 const registerNewUser = async (email, password) => {
-    return axios.post('http://localhost:5000/api/v1/register', {
-        email,
-        password,
-    });
+    try {
+        return axios.post('http://localhost:5000/api/v1/register', {
+            email,
+            password,
+        });
+    } catch (error) {
+        console.log(error);
+        throw new Error(error);
+    }
+};
+const login = async (email, password) => {
+    try {
+        return axios.post('http://localhost:5000/api/v1/login', {
+            email,
+            password,
+        });
+    } catch (error) {
+        console.log(error);
+        throw new Error(error);
+    }
 };
 // ===========   book api   ===========
 const getBooks = async () => {
-    return axios.get('http://localhost:5000/api/v1/book/read');
+    try {
+        return axios.get('http://localhost:5000/api/v1/book/read');
+    } catch (error) {
+        console.log(error);
+        throw new Error(error);
+    }
 };
 const getBookById = async (id) => {
-    return axios.get(`http://localhost:5000/api/v1/book/read/${id}`);
+    try {
+        return axios.get(`http://localhost:5000/api/v1/book/read/${id}`);
+    } catch (error) {
+        console.log(error);
+        throw new Error(error);
+    }
 };
 export { getBooks, getBookById, registerNewUser };
