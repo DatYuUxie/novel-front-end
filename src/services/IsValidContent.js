c1d37c4104092dad71b0aeb9055f1b951ed7d431
DatYuUxie <118263835+DatYuUxie@users.noreply.github.com>
2024-05-29 11:10:21 +0700

    valid content

--- src/services/IsValidContent.js
+++ src/services/IsValidContent.js
@@ -3,8 +3,7 @@ function IsValidContent(text) {
     const linkRegex = /(http|https):\/\/[^\s]+/gi;
 
     // Biểu thức chính quy để kiểm tra từ tục tỉu
-    const obsceneWords = ['tục', 'tỉu', 'lờ', 'đụ', 'cc', 'đm', 'đmm', 'duma'];
-    // const obsceneWords = ['tục duma'];
+    const obsceneWords = ['ffff'];
 
     const obsceneRegex = new RegExp(obsceneWords.join('|'), 'gi');
 
